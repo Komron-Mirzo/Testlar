9e79a1880b8a74c6c685729077a9504abf521b58
Komron-Mirzo <kabduvaliyev@gmail.com>
2022-10-17 00:28:18 +0500

    Assets added

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:untitled/data_uploader_screen.dart';
 import 'package:untitled/firebase_options.dart';
+import 'package:get/get.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(Testlar());
-}
-
-class Testlar extends StatelessWidget {
-  const Testlar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: SafeArea(
-          child: Container(
-            child: Center(
-              child: Text("Salom hehey"),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(GetMaterialApp(home: DataUploaderScreen()));
 }
